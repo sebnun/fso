1c2fcf64911aa968a05a8e17d22477cfb0d89037
Pablo <pablo@injavascript.com>
2021-08-12 11:08:22 +0200

    1b

--- part1/firstapp/src/App.js
+++ part1/firstapp/src/App.js
@@ -1,42 +1,52 @@
 import React from 'react'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
 
-const Header = ({ course }) =>
-  <h1>{course}</h1>
+const Header = ({ name }) =>
+  <h1>{name}</h1>
 
 
-const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) =>
+const Content = ({ parts }) =>
   <>
-    <Part exercises={exercises1} part={part1} />
-    <Part exercises={exercises2} part={part2} />
-    <Part exercises={exercises3} part={part3} />
+    <Part part={parts[0]} />
+    <Part part={parts[1]} />
+    <Part part={parts[2]} />
   </>
 
 
-const Total = ({ exercises1, exercises2, exercises3 }) =>
-  <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+const Total = ({ parts }) =>
+  <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
 
 
-const Part = ({ exercises, part }) =>
+const Part = ({ part }) =>
   <p>
-    {part} {exercises}
+    {part.name} {part.exercises}
   </p>
 
 
